a4c4d0de63f72083121b99f546ffddb61625838b
asmabegum22 <asmabegum.g22@gmail.com>
2026-07-31 01:51:45 +0530

    Added complete frontend code

---
+++ frontend/src/app/pages/project-manager/Logout/Logout/pm-logout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pm-logout',
+  imports: [],
+  templateUrl: './pm-logout.html',
+  styleUrl: './pm-logout.css',
+})
+export class PmLogout {}
