32ba1cb16dd34d42d7a8087851753ca436c30415
Candidclover <Sonali.so2006@gmail.com>
2026-08-04 23:54:10 +0530

    Add project management module and update gitignore

--- alembic/env.py
+++ alembic/env.py
@@ -1,20 +1,24 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import os
+from dotenv import load_dotenv
 
+from database import Base
+import app.models.user
+
+load_dotenv()
+               
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-database_url = os.getenv("DATABASE_URL").replace("%", "%%")
-config.set_main_option("sqlalchemy.url", database_url)
-
+config.set_main_option(
+    "sqlalchemy.url",
+    os.getenv("DATABASE_URL").replace("%","%%")
+)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -24,9 +28,6 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from database import Base
-import models
-
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
--- auth.py
+++ auth.py
@@ -1,7 +1,12 @@
+from fastapi import HTTPException
+from jose import JWTError
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+security = HTTPBearer()
 import os
 
 load_dotenv()
@@ -21,8 +26,64 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def create_access_token(data: dict):
+# credentials: HTTPAuthorizationCredentials = Depends(security)
+# ...
+# token = credentials.credentials
+
+
+def create_access_token(data: dict, remember_me: bool = False):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    if remember_me:
+        expire = datetime.utcnow() + timedelta(days=30)
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    print("Remember Me:", remember_me)
+    print("Expires At:", expire)
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        email = payload.get("sub")
+
+        if email is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid token"
+            )
+
+        return email
+
+    except JWTError:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid or expired token"
+        )
+from datetime import datetime, timedelta
+from jose import jwt
+
+EMAIL_SECRET = SECRET_KEY
+
+def create_email_verification_token(email: str):
+    expire = datetime.utcnow() + timedelta(hours=24)
+
+    payload = {
+        "sub": email,
+        "exp": expire
+    }
+
+    return jwt.encode(payload, EMAIL_SECRET, algorithm=ALGORITHM)    
